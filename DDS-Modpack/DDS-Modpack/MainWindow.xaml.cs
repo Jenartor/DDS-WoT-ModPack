5bb617dcf666bb1f1342e843e8f88d834ae10ad8
Jenartor <jenartor@googlemail.com>
2015-05-19 21:57:37 +0200

    RC 1
    Commit of the final Changes for an Release Canditate, which ich fully
    working.

--- DDS-Modpack/DDS-Modpack/MainWindow.xaml.cs
+++ DDS-Modpack/DDS-Modpack/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Net;
+using System.IO;
+using System.IO.Compression;
 
 namespace DDS_Modpack {
     /// <summary>
@@ -22,9 +24,10 @@ namespace DDS_Modpack {
     public partial class MainWindow : Window {
         FolderBrowserDialog dialog = new FolderBrowserDialog();
         string filename = "vehicles.zip", myStringWebResource = null;
-        string remoteURL = "";
+        string remoteURL = "https://github.com/Jenartor/DDS-WoT-ModPack/blob/master/vehicles.zip?raw=true";
         public MainWindow() {
             InitializeComponent();
+            btn_folder.Visibility = System.Windows.Visibility.Hidden;
             txtbox_welcome.Text = "Willkommen beim DDS-Modpack Installer. Bitte drücken Sie den Button '" + btn_next.Content + "' um fortzufahren!";
         }
 
@@ -35,7 +38,7 @@ namespace DDS_Modpack {
         private void btn_next_Click(object sender, RoutedEventArgs e) {
             lbl_download.Content = "Download angefangen";
             using (WebClient client = new WebClient()) {
-                myStringWebResource = remoteURL + filename;
+                myStringWebResource = remoteURL;
                 client.DownloadFile(myStringWebResource, filename);
             }
             lbl_download.Content = "Download abgeschlossen";
@@ -50,6 +53,24 @@ namespace DDS_Modpack {
                 btn_install.Visibility = System.Windows.Visibility.Visible;
                 txtbox_welcome.Text = "Sie haben den Ordner '" + dialog.SelectedPath + "' gewählt. Bitte überprüfen Sie die Korrektheit, bevor sie auf '" + btn_install.Content + "' drücken.";
             }
+        }
+
+        private void btn_install_Click(object sender, RoutedEventArgs e) {
+            extract();
+        }
+        private void extract() {
+            string mods = "res_mods\\0.9.7\\";
+            string extractPath = System.IO.Path.Combine(dialog.SelectedPath, mods);
+            using (ZipArchive myziparchive = ZipFile.OpenRead(filename)) {
+                foreach (ZipArchiveEntry entry in myziparchive.Entries) {
+                    if (!entry.FullName.EndsWith(".dds", StringComparison.OrdinalIgnoreCase)) {
+                        System.IO.Directory.CreateDirectory(extractPath + entry.FullName.Substring(0, entry.FullName.Length - 1));
+                    } else {
+                        entry.ExtractToFile(System.IO.Path.Combine(extractPath + entry.FullName), true);
+                    }
+                }
+            }
+            txtbox_welcome.Text = "Fertig. Nun starten Sie World of Tanks und genießen Sie das DDS-Modpack!";
         }
     }
 }
